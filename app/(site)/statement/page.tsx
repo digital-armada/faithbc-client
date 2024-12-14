c28ee0be3e971a913f7458d812d185f644d3ce5c
Steven <steven@digitalarmada.com.au>
2024-12-14 16:30:11 +1100

    fix statements

--- app/(site)/statement/page.tsx
+++ app/(site)/statement/page.tsx
@@ -3,25 +3,25 @@ import { getStatements } from "@/data/doctrines";
 import Link from "next/link";
 
 export default async function Page() {
-  const { data = [] } = (await getStatements()) || { data: [] };
+  const { data } = await getStatements();
 
   return (
     <section>
       <div className="container">
         <PageHeader heading="Statement of Faith" />
         <ul role="list" className="flex flex-col divide-y divide-gray-700/10">
-          {data.map((statement) => (
-            <Link
-              key={statement?.id || "default-key"}
-              href={`statement/${statement?.attributes?.slug || "#"}`}
-            >
-              <li className="py-4">
-                <p className="text-gray-700">
-                  {statement?.attributes?.title || "Untitled Statement"}
-                </p>
-              </li>
-            </Link>
-          ))}
+          {data.map((statement: any) => {
+            return (
+              <Link
+                key={statement.id}
+                href={`statement/${statement?.attributes?.slug}`}
+              >
+                <li className="py-4">
+                  <p className="text-gray-700">{statement.attributes.title}</p>
+                </li>
+              </Link>
+            );
+          })}
         </ul>
       </div>
     </section>
--- data/doctrines.ts
+++ data/doctrines.ts
@@ -1,17 +1,11 @@
-import axios from "axios";
-
 export async function getStatement(slug: string) {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/doctrines/${slug}`,
-      {
-        next: { revalidate: 60 },
-      },
     );
     if (res.status !== 200) {
       throw new Error("Failed to fetch statements");
     }
-
     return res.json();
   } catch (error) {
     throw new Error("Failed to fetch statements");
@@ -22,9 +16,6 @@ export async function getStatements() {
   try {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/doctrines?pagination[limit]=30`,
-      {
-        next: { revalidate: 60 },
-      },
     );
 
     if (res.status !== 200) {
