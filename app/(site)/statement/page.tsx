3ad35755f810c0b7973283a23fe2727c71df7497
Steven <steven@digitalarmada.com.au>
2024-12-14 17:31:01 +1100

    fix statements again again

--- app/(site)/statement/page.tsx
+++ app/(site)/statement/page.tsx
@@ -4,24 +4,26 @@ import Link from "next/link";
 
 export default async function Page() {
   const { data } = await getStatements();
-
   return (
     <section>
       <div className="container">
         <PageHeader heading="Statement of Faith" />
         <ul role="list" className="flex flex-col divide-y divide-gray-700/10">
-          {data.map((statement: any) => {
-            return (
-              <Link
-                key={statement.id}
-                href={`statement/${statement?.attributes?.slug}`}
-              >
-                <li className="py-4">
-                  <p className="text-gray-700">{statement.attributes.title}</p>
-                </li>
-              </Link>
-            );
-          })}
+          {data &&
+            data?.data.map((statement: any) => {
+              return (
+                <Link
+                  key={statement.id}
+                  href={`statement/${statement?.attributes?.slug}`}
+                >
+                  <li className="py-4">
+                    <p className="text-gray-700">
+                      {statement.attributes.title}
+                    </p>
+                  </li>
+                </Link>
+              );
+            })}
         </ul>
       </div>
     </section>
--- data/doctrines.ts
+++ data/doctrines.ts
@@ -1,7 +1,6 @@
 import { strapiRequest } from "@/lib/strapi-service";
 
 export async function getStatement(slug: string) {
-  console.log("SLUG", slug);
   try {
     const response = await strapiRequest("GET", `/doctrines/${slug}`);
     return response;
@@ -12,16 +11,26 @@ export async function getStatement(slug: string) {
 
 export async function getStatements() {
   try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/doctrines?pagination[limit]=30`,
+    const response = await strapiRequest(
+      "GET",
+      `/doctrines?pagination[limit]=30`,
     );
-
-    if (res.status !== 200) {
-      throw new Error("Failed to fetch statements");
-    }
-
-    return res.json();
+    return response;
   } catch (error) {
     throw new Error("Failed to fetch statements");
   }
+
+  //   try {
+  //     const res = await fetch(
+  //       `${process.env.NEXT_PUBLIC_API_URL}/doctrines?pagination[limit]=30`,
+  //     );
+  //
+  //     if (res.status !== 200) {
+  //       throw new Error("Failed to fetch statements");
+  //     }
+  //
+  //     return res.json();
+  //   } catch (error) {
+  //     throw new Error("Failed to fetch statements");
+  //   }
 }
