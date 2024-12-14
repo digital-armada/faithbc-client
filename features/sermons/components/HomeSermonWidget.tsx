ef950234799fd3347e3ae8db8775b5554d4d9153
Steven <steven@digitalarmada.com.au>
2024-12-14 16:45:47 +1100

    fix sermons

--- features/sermons/components/HomeSermonWidget.tsx
+++ features/sermons/components/HomeSermonWidget.tsx
@@ -2,18 +2,18 @@ import HomeSermonCard from "./HomeSermonCard";
 import { sermonsService } from "../sermons-service";
 
 export default async function HomeSermonWidget() {
-  const { data } = await sermonsService.getSermons({
+  const { data = { data: [] } } = (await sermonsService.getSermons({
     pageSize: 4,
-  });
+  })) || { data: { data: [] } };
 
-  const sermons = data?.data;
+  const sermons = data?.data || [];
 
   return (
     <section>
       <div className="container mt-10 w-full space-y-4 font-body text-sm">
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-4">
           {sermons.map((sermon) => (
-            <HomeSermonCard sermon={sermon} key={sermon.id} />
+            <HomeSermonCard sermon={sermon} key={sermon?.id || Math.random()} />
           ))}
         </div>
       </div>
