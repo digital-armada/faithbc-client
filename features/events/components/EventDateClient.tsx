4b005b7a1a31334789b40109d8bab5140e5f14c3
Steven <steven@digitalarmada.com.au>
2024-12-19 21:14:53 +1100

    temp event test

--- features/events/components/EventDateClient.tsx
+++ features/events/components/EventDateClient.tsx
@@ -1,9 +1,16 @@
 "use client";
 import { DateTime } from "luxon";
+import { useEffect, useState } from "react";
 export default function EventDateClient({ startDate, endDate }) {
-  console.log("startDate", startDate);
-  const startDateObj = DateTime.fromISO(startDate, { zone: "utc" })
-    .setZone("Australia/Sydney")
-    .toFormat("EEE, d MMM, h:mm a");
-  return <div>{startDateObj}</div>;
+  const [date, setDate] = useState("");
+  useEffect(() => {
+    const date = new Date(startDate);
+    setDate(date.toLocaleString());
+  }, [startDate]);
+
+  // console.log("startDate", startDate);
+  // const startDateObj = DateTime.fromISO(startDate, { zone: "utc" })
+  //   .setZone("Australia/Sydney")
+  //   .toFormat("EEE, d MMM, h:mm a");
+  return <div>{date}</div>;
 }
